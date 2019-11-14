45b4abca22ae29fec8d983029fd524e700fe0c21
laravel <1255949790@qq.com>
2019-11-14 07:29:28 +0000

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,14 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    //话题与分类一对一
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    //一个话题属于一个作者
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
