de7b226d851701af60e9446ae338dc54a49f0be8
laravel <1255949790@qq.com>
2019-11-19 05:41:14 +0000

    用户可以上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,6 @@ Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'crea
 
 //分类相关路由
 Route::resource('categories','CategoriesController',['only'=>['show']]);
+
+//上传图片的URL
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
