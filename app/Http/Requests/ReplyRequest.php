b6b63363f500e087ba0ebf2a7dd1ee66ff874099
laravel <1255949790@qq.com>
2019-11-21 03:31:48 +0000

    用户可以发表评论

--- app/Http/Requests/ReplyRequest.php
+++ app/Http/Requests/ReplyRequest.php
@@ -6,36 +6,16 @@ class ReplyRequest extends Request
 {
     public function rules()
     {
-        switch($this->method())
-        {
-            // CREATE
-            case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
-            // UPDATE
-            case 'PUT':
-            case 'PATCH':
-            {
-                return [
-                    // UPDATE ROLES
-                ];
-            }
-            case 'GET':
-            case 'DELETE':
-            default:
-            {
-                return [];
-            }
-        }
+        return [
+            // 创建规则
+            'content' => 'required|min:2',
+        ];
     }
 
     public function messages()
     {
         return [
-            // Validation messages
+            // 验证信息
         ];
     }
 }
